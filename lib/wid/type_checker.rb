8b9797dcdca5587a465d1862b459f55038af3997
Piotr Usewicz <piotr@layer22.com>
2024-10-19 10:54:13 +0200

    Update type_checker.rb

--- lib/wid/type_checker.rb
+++ lib/wid/type_checker.rb
@@ -1,3 +1,38 @@
+# frozen_string_literal: true
+# typed: strict
+
+require_relative "type"
+
+require "bigdecimal"
+
+module Wid
+  class TypeChecker
+    extend T::Sig
+
+    sig { params(input: Object).returns(Type) }
+    def typecheck(input)
+      case input
+      when Integer then return Type::Int
+      when Float then return Type::Float
+      when BigDecimal then return Type::Double
+      end
+
+      if string?(input)
+        return Type::String
+      end
+
+      raise "Unknown type for `#{input}'"
+    end
+
+    sig { params(input: Object).returns(Type) }
+    def self.typecheck(input)
+      new.typecheck(input)
+    end
+
+    private
+
+    sig { params(input: Object).returns(T::Boolean) }
+    def string?(input)
       return false unless input.is_a?(String)
       return input.end_with?('"') if input.start_with?('"')
       return input.end_with?("'") if input.start_with?("'")
